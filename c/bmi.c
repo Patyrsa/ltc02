93f242e43f4c562df1a414bf6ba09647269657f7
EVoronina <malyavochka33@gmail.com>
2019-03-19 23:06:57 +0300

    last commit

--- c/bmi.c
+++ c/bmi.c
@@ -1,18 +1,19 @@
 #include "stdio.h"
 
 int main (void) {
-    int height, weight;
-    float BMI;
-
-    printf("Your height?(sm)\n");
-    scanf("%d", &height);
+    int weight;
+    float BMI, height;
 
     printf("Your weight?(kg)\n");
     scanf("%d", &weight);
 
-    BMI = weight/(height*height);
-
+    printf("Your height?(m)\n");
+    scanf("%f", &height);
+   
+    BMI = weight/(float)(height*height);
+    
     printf("Your BMI is = %2.2f\n", BMI);
+    
 
 //норма BMI 18.5 - 24.99 (wiki)
 
