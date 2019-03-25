3f1bf33696a1f82ddda5f8aca8b75c02a30aed98
Patyrsa <patyrsa@gmail.com>
2019-03-25 16:07:02 +0300

    Update palindrome2.js

--- js/palindrome2.js
+++ js/palindrome2.js
@@ -1,15 +1,20 @@
 function isPalindrome(text) {
-    var str = text.toLowerCase().split(/[^a-z]*/).join("");
-//     console.log(str);
-    if(str.length >= 2){
-//         console.log("str >=2");
-        if(str[0]==str[str.length - 1]) {    
-            return isPalindrome(str.slice(1,str.length - 1));
-        } else {
-            return false;
-        }
-    }else{
+    var characters = "abcdifghijklmnopqrstuvwxyz";
+    var str = text.toLowerCase();
+    if(str.length < 2){
         return true;
-    }
+    } else {
+        if(characters.indexOf(str[0])<0) {
+            return isPalindrome(str.substr(1,str.length - 1));
+        }else if(characters.indexOf(str[str.length - 1])<0) {
+			return isPalindrome(str.substr(0,str.length - 1));
+        }else if(str[0]==str[str.length - 1]){
+			return isPalindrome(str.substr(1,str.length - 2));
+        }else{
+            return false;
+        }     
+    }   
 }
-console.log(isPalindrome("A man, a plan, a canal. Panama."));
+console.log(isPalindrome("Not New \\York, Roy went on."));
+console.log(isPalindrome("A man, a plan a canal-Panama"));
+console.log(isPalindrome("jkjlkjlkjgg"));
