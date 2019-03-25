ff13e305c7a6e85555f1f29a8f04ede3beac4576
Patyrsa <patyrsa@gmail.com>
2019-03-25 06:44:20 +0300

    Update palindrome2.js

--- js/palindrome2.js
+++ js/palindrome2.js
@@ -1,10 +1,15 @@
 function isPalindrome(text) {
-    var textLen = text.lenght;
-    var textLC = text.split(" \n.,;:!?-_+-@#$%^&*,'<>`~()[]{}").join("").toLowerCase();
-if(textLC==text) {
-    return true
-} else {
-    return false
-}
+    var str = text.toLowerCase().split(/[^a-z]*/).join("");
+//     console.log(str);
+    if(str.length >= 2){
+//         console.log("str >=2");
+        if(str[0]==str[str.length - 1]) {    
+            return isPalindrome(str.slice(1,str.length - 1));
+        } else {
+            return false;
+        }
+    }else{
+        return true;
+    }
 }
 console.log(isPalindrome("A man, a plan, a canal. Panama."));
